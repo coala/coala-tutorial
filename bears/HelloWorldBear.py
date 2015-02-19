f29962677c5d02debadc59ffb9b2ca4335cdf875
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-02-19 17:13:14 +0100

    Add HelloWorldBear
    Will be used for the bear writing tutorial.

---
+++ bears/HelloWorldBear.py
@@ -0,0 +1,8 @@
+from coalib.bears.LocalBear import LocalBear
+
+
+class HelloWorldBear(LocalBear):
+    def run_bear(self,
+                 filename,
+                 file):
+        self.debug_msg("Hello World!")
