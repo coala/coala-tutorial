ae96bf9dc7206003b3e14b884e227c7495ebb07a
Saurabh Sandav <justmailsaurabh@gmail.com>
2016-08-18 12:21:14 +0530

    src/main.c: Remove unnecessary space

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h> 
 
 int main(void) {
-    printf("Welcome to coala. Keep following the tutorial, you are doing a great job so far !\n");
+    printf("Welcome to coala. Keep following the tutorial, you are doing a great job so far!\n");
 
     return 0;
 }
